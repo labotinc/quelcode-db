f6c5b663f27e246b982c8a3c4354b35d34f4249a
Kako Fujimoto <ayaka20111005@yahoo.co.jp>
2020-05-29 18:31:45 +0900

    それぞれのテーブルのPKのカラム名を単なる「id」に変更

--- db/db-challenge2/ddl.sql
+++ db/db-challenge2/ddl.sql
@@ -1,6 +1,6 @@
 create table users
 (
-    user_id int(11) not null auto_increment,
+    id int(11) not null auto_increment,
     email varchar(100) not null,
     password varchar(100) not null,
     user_name varchar(100) not null,
@@ -17,7 +17,7 @@ create table users
 
 create table chat_rooms
 (
-    chat_id int(11) not null auto_increment,
+    id int(11) not null auto_increment,
     chat_name varchar(100) not null, 
     chat_detail varchar(1000),
     file_send tinyint(1) not null default 0,
@@ -27,8 +27,8 @@ create table chat_rooms
     created_at datetime not null,
     update_user_id int(11) not null,
     updated_at datetime not null,
-    foreign key(create_user_id) references users(user_id),
-    foreign key(update_user_id) references users(user_id),
+    foreign key(create_user_id) references users(id),
+    foreign key(update_user_id) references users(id),
     primary key(chat_id)
 )ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
@@ -37,14 +37,14 @@ create table chat_members
     chat_id int(11) not null,
     user_id int(11) not null,
     join_time datetime not null,
-    foreign key(chat_id) references chat_rooms(chat_id),
-    foreign key(user_id) references users(user_id),
-    primary key(chat_id, user_id)
+    foreign key(chat_id) references chat_rooms(id),
+    foreign key(user_id) references users(id),
+    primary key(chat_id, id)
 )ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 create table tasks
 (
-    task_id int(11) auto_increment not null,
+    id int(11) auto_increment not null,
     chat_id int(11) not null,
     task_detail varchar(1000) not null,
     user_id int(11) not null,
@@ -55,16 +55,16 @@ create table tasks
     update_user_id int(11) not null,
     updated_at datetime not null,
     is_deleted tinyint(1) not null default 0,
-    foreign key(chat_id) references chat_rooms(chat_id),
-    foreign key(user_id) references users(user_id),
-    foreign key(create_user_id) references users(user_id),
-    foreign key(update_user_id) references users(user_id),
+    foreign key(chat_id) references chat_rooms(id),
+    foreign key(user_id) references users(id),
+    foreign key(create_user_id) references users(id),
+    foreign key(update_user_id) references users(id),
     primary key(task_id)
 )ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 create table posts
 (
-    post_id int(11) auto_increment not null,
+    id int(11) auto_increment not null,
     chat_id int(11) not null,
     post_detail varchar(1000) not null,
     user_id int(11) not null,
@@ -73,8 +73,8 @@ create table posts
     updated_at datetime not null,
     is_deleted tinyint(1) not null default 0,
     add_dile varchar(100) not null,
-    foreign key(chat_id) references chat_rooms(chat_id),
-    foreign key(user_id) references users(user_id),
-    foreign key(update_user_id) references users(user_id),
+    foreign key(chat_id) references chat_rooms(id),
+    foreign key(user_id) references users(id),
+    foreign key(update_user_id) references users(id),
     primary key(post_id)    
 )ENGINE=InnoDB  DEFAULT CHARSET=utf8;
