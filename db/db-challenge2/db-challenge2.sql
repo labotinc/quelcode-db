af07460a8df38f30f89ea1293561af2c1c1fd271
MUnehiroMachida <Life1231@outlook.jp>
2020-07-03 22:39:10 +0900

    7/3レビュー後修正

--- db/db-challenge2/db-challenge2.sql
+++ db/db-challenge2/db-challenge2.sql
@@ -1,27 +1,27 @@
 CREATE TABLE `user_information` (
     `id` int(11) PRIMARY KEY AUTO_INCREMENT,
-    `email_address` varchar(100) NOT NULL UNIQUE,
+    `email_address` varchar(100) NOT NULL,
     `password` varchar(100) NOT NULL,
     `display_name` varchar(100) NOT NULL,
     `profile` varchar(1000),
-    `workplace_phone_number` varchar(13),
-    `myphone_number` varchar(13) UNIQUE,
-    `account_delete_flag` tinyint(1) NOT NULL DEFAULT '0',
-    `create_date_and_time` datetime NOT NULL,
-    `update_date_and_time`datetime NOT NULL
+    `office_telephone_number` varchar(13),
+    `mobilephone_number` varchar(13),
+    `is_deleted` tinyint(1) NOT NULL DEFAULT '0',
+    `created_at` datetime NOT NULL,
+    `updated_at` datetime NOT NULL
 );
 
 CREATE TABLE `chatroom` (
-    `chatroom_id` int(11) PRIMARY KEY AUTO_INCREMENT,
+    `id` int(11) PRIMARY KEY AUTO_INCREMENT,
     `chatroom_name` varchar(100) NOT NULL,
     `chatroom_overview` varchar(1000),
     `file_transmission_flag` tinyint(1) NOT NULL DEFAULT '0',
     `direct_chat_flag` tinyint(1) NOT NULL DEFAULT '0',
-    `chatroom_delete_flag` tinyint(1) NOT NULL DEFAULT '0',
-    `create_author` int(11) NOT NULL REFERENCES user_information(id),
-    `update_author` int(11) NOT NULL REFERENCES user_information(id),
-    `create_date_and_time` datetime NOT NULL,
-    `update_date_and_time`datetime NOT NULL
+    `is_deleted` tinyint(1) NOT NULL DEFAULT '0',
+    `create_user` int(11) NOT NULL REFERENCES user_information(id),
+    `update_user` int(11) NOT NULL REFERENCES user_information(id),
+    `created_at` datetime NOT NULL,
+    `updated_at` datetime NOT NULL
 );
 
 CREATE TABLE `participant` (
@@ -32,27 +32,27 @@ CREATE TABLE `participant` (
 );
 
 CREATE TABLE `posts` (
-    `post_id` int(11) PRIMARY KEY AUTO_INCREMENT,
+    `id` int(11) PRIMARY KEY AUTO_INCREMENT,
     `chatroom_id` int(11) NOT NULL REFERENCES chatroom(chatroom_id),
     `post_content` varchar(1000) NOT NULL,
     `file_name` varchar(100),
-    `post_content_delete_flag` tinyint(1) NOT NULL DEFAULT '0',
-    `create_author` int(11) NOT NULL REFERENCES user_information(id),
-    `update_author` int(11) NOT NULL REFERENCES user_information(id),
-    `create_date_and_time` datetime NOT NULL,
-    `update_date_and_time`datetime NOT NULL
+    `is_deleted` tinyint(1) NOT NULL DEFAULT '0',
+    `create_user` int(11) NOT NULL REFERENCES user_information(id),
+    `update_user` int(11) NOT NULL REFERENCES user_information(id),
+    `created_at` datetime NOT NULL,
+    `updated_at` datetime NOT NULL
 );
 
 CREATE TABLE `task` (
-    `task_id` int(11) PRIMARY KEY AUTO_INCREMENT,
+    `id` int(11) PRIMARY KEY AUTO_INCREMENT,
     `chatroom_id` int(11) NOT NULL REFERENCES chatroom(chatroom_id),
     `task_content` varchar(1000) NOT NULL,
     `person_in_charge_id` int(11) NOT NULL REFERENCES user_information(id),
-    `deadline` datetime(1),
+    `deadline` datetime,
     `complete_flag` tinyint(1) NOT NULL DEFAULT '0',
-    `post_content_delete_flag` tinyint(1) NOT NULL DEFAULT '0',
-    `create_author` int(11) NOT NULL REFERENCES user_information(id),
-    `update_author` int(11) NOT NULL REFERENCES user_information(id),
-    `create_date_and_time` datetime NOT NULL,
-    `update_date_and_time`datetime NOT NULL
+    `is_deleted` tinyint(1) NOT NULL DEFAULT '0',
+    `create_user` int(11) NOT NULL REFERENCES user_information(id),
+    `update_user` int(11) NOT NULL REFERENCES user_information(id),
+    `created_at` datetime NOT NULL,
+    `updated_at` datetime NOT NULL
 );
