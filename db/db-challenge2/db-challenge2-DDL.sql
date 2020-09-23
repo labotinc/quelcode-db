43b105523ec268544b22a2c0efe431e5727c9cca
Satu <kttth0320@gmail.com>
2020-09-23 17:02:37 +0900

    データベース定義書の転記漏れ修正

--- db/db-challenge2/db-challenge2-DDL.sql
+++ db/db-challenge2/db-challenge2-DDL.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `users` (
-      `id` int NOT NULL AUTO_INCREMENT,
+      `id` int(11) NOT NULL AUTO_INCREMENT,
       `mail` varchar(100) NOT NULL,
       `password` varchar(100) NOT NULL,
       `name` varchar(100) NOT NULL,
@@ -7,57 +7,57 @@ CREATE TABLE `users` (
       `number_work` varchar(13),
       `number_private` varchar(13),
       `is_deleted` tinyint(1) DEFAULT 0 NOT NULL comment'1:削除済み',
-      `created_at` datetime NOT NULL,
-      `modified_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+      `created_at` datetime(5) DEFAULT CURRENT_TIMESTAMP NOT NULL,
+      `modified_at` timestamp(4) DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL,
       PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
 
 CREATE TABLE `chat_users` (
-      `user_id` int NOT NULL REFERENCES users(ID),
-      `chat_id` int NOT NULL REFERENCES chats(ID),
-      `joined_at` datetime NOT NULL,
+      `user_id` int(11) NOT NULL REFERENCES users(ID),
+      `chat_id` int(11) NOT NULL REFERENCES chats(ID),
+      `joined_at` datetime(5) DEFAULT CURRENT_TIMESTAMP NOT NULL,
       PRIMARY KEY (`user_id`,`chat_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
 
 CREATE TABLE `chats` (
-      `id` int NOT NULL AUTO_INCREMENT,
+      `id` int(11) NOT NULL AUTO_INCREMENT,
       `name` varchar(100) NOT NULL,
       `summary` varchar(1000),
-      `is_good_to_send` tinyint(1) DEFAULT 0 NOT NULL comment'1:ファイル送信可能',
-      `is_good_to_start_direct` tinyint(1) DEFAULT 0 NOT NULL comment'1:ダイレクトチャット開始',
+      `is_sendable` tinyint(1) DEFAULT 0 NOT NULL comment'1:ファイル送信可能',
+      `is_directchat` tinyint(1) DEFAULT 0 NOT NULL comment'1:ダイレクトチャット開始',
       `is_deleted` tinyint(1) DEFAULT 0 NOT NULL comment'1:削除済み',
-      `created_at` datetime NOT NULL,
-      `created_by` int NOT NULL REFERENCES users(ID),
-      `modified_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-      `modified_by` int NOT NULL REFERENCES users(ID),
+      `created_at` datetime(5) DEFAULT CURRENT_TIMESTAMP NOT NULL,
+      `created_by` int(11) NOT NULL REFERENCES users(ID),
+      `modified_at` timestamp(4) DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL,
+      `modified_by` int(11) NOT NULL REFERENCES users(ID),
       PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
 
 CREATE TABLE `posts` (
-      `id` int NOT NULL AUTO_INCREMENT,
-      `chat_id` int NOT NULL REFERENCES chats(ID),
+      `id` int(11) NOT NULL AUTO_INCREMENT,
+      `chat_id` int(11) NOT NULL REFERENCES chats(ID),
       `contents` varchar(1000) NOT NULL,
       `file_name` varchar(100),
       `is_deleted` tinyint(1) DEFAULT 0 NOT NULL comment'1:削除済み',
-      `created_at` datetime NOT NULL,
-      `created_by` int NOT NULL REFERENCES users(ID),
-      `modified_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-      `modified_by` int NOT NULL REFERENCES users(ID),
+      `created_at` datetime(5) DEFAULT CURRENT_TIMESTAMP NOT,
+      `created_by` int(11) NOT NULL REFERENCES users(ID),
+      `modified_at` timestamp(4) DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL,
+      `modified_by` int(11) NOT NULL REFERENCES users(ID),
       PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
 
 CREATE TABLE `tasks` (
-      `id` int NOT NULL AUTO_INCREMENT,
+      `id` int(11) NOT NULL AUTO_INCREMENT,
       `contents` varchar(1000) NOT NULL,
-      `pic_id` int NOT NULL REFERENCES users(ID),
-      `chat_id` int NOT NULL REFERENCES chats(ID),
-      `deadline` datetime,
+      `person_in_charge` int(11) NOT NULL REFERENCES users(ID),
+      `chat_id` int(11) NOT NULL REFERENCES chats(ID),
+      `deadline` datetime(5),
       `is_completed` tinyint(1) DEFAULT 0 NOT NULL comment'1:完了済み',
       `is_deleted` tinyint(1) DEFAULT 0 NOT NULL comment'1:削除済み',
-      `created_at` datetime NOT NULL,
-      `created_by` int NOT NULL REFERENCES users(ID),
-      `modified_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-      `modified_by` int NOT NULL REFERENCES users(ID),
+      `created_at` datetime(5) DEFAULT CURRENT_TIMESTAMP NOT NULL,
+      `created_by` int(11) NOT NULL REFERENCES users(ID),
+      `modified_at` timestamp(4) DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL,
+      `modified_by` int(11) NOT NULL REFERENCES users(ID),
       PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
 
